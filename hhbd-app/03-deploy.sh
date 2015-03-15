016f6df6f107887f9cee10c33f908d90fe653f64
Jakub Kułak <jakub.kulak@gmail.com>
2015-03-15 23:41:41 +0100

    Update app deployment script

--- hhbd-app/03-deploy.sh
+++ hhbd-app/03-deploy.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-set -ex
+# Define target server for selected branch - done in Strider Environment Variables
+# TARGET_SERVER='new.hhbd.pl'
 
-if [ "$#" -ne 2 ]
-then
-    echo "This script requires two parameters: target_server and deployer_user_name"
-    exit 1
-fi
+# Define target directory - done in Strider Environment Variables
+# TARGET_DIR=/var/www/new.hhbd.pl
+
+set -ex
 
 # get the time - this will be used as the name of the directory
 TIME_TAG=`date +"%y%m%d_%H%M%S"`
 PACKAGE_NAME=package-hhbd-app-$TIME_TAG.tar.gz
-TARGET_SERVER=$1
-DEPLOYMENT_USER=$2
-
-echo "$DEPLOYMENT_USER deploys $TIME_TAG to $TARGET_SERVER"
 
 # Compress all content of the directory
 tar -czf /tmp/$PACKAGE_NAME *
 
-TARGET_DIR=/var/www/hhbd-app
-
 # Copy package
-scp /tmp/$PACKAGE_NAME $DEPLOYMENT_USER@$TARGET_SERVER:/tmp
+scp /tmp/$PACKAGE_NAME $TARGET_SERVER:/tmp
 
 # Run multiple commands over SSH
 ssh $DEPLOYMENT_USER@$TARGET_SERVER /bin/bash << EOF
@@ -34,7 +28,7 @@ ssh $DEPLOYMENT_USER@$TARGET_SERVER /bin/bash << EOF
     tar xzf /tmp/$PACKAGE_NAME -C $TARGET_DIR/releases/$TIME_TAG
 
     # Copy configuration from current
-    cp $TARGET_DIR/releases/current/config/config.local.neon $TARGET_DIR/releases/$TIME_TAG/config/config.local.neon
+    cp $TARGET_DIR/releases/current/application/configs/application.ini $TARGET_DIR/releases/$TIME_TAG/application/configs/application.ini
 
     # Remove previous
     rm -r $TARGET_DIR/releases/previous
