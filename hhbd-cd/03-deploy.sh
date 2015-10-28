9e04bd61464e2a7225317720c91e887a4ff011c4
Jakub Kułak <jakub.kulak@gmail.com>
2015-10-28 15:37:16 +0100

    Update deployment scrip

--- hhbd-cd/03-deploy.sh
+++ hhbd-cd/03-deploy.sh
@@ -2,22 +2,30 @@
 
 set -ex
 
+if [ "$#" -ne 1 ]
+then
+    echo "This script requires one parameter: TARGET_DIR"
+    exit 1
+fi
+
 # get the time - this will be used as the name of the directory
 TIME_TAG=`date +"%y%m%d_%H%M%S"`
 
 echo "Copying deployment scripts where they belong (v. $TIME_TAG)"
 
-TARGET_DIR=/home/strider
+TARGET_DIR=$1
+
+echo "TARGET_DIR: $TARGET_DIR"
 
 # Move scripts only to new release directory
-mkdir -p $TARGET_DIR/scripts/releases/$TIME_TAG
-mv hhbd-* $TARGET_DIR/scripts/releases/$TIME_TAG
+mkdir -p $TARGET_DIR/releases/$TIME_TAG
+mv hhbd-* $TARGET_DIR/releases/$TIME_TAG
 
 # Remove previous
-rm $TARGET_DIR/scripts/releases/previous
+rm $TARGET_DIR/releases/previous
 
 # Move current to previous
-cp -r $TARGET_DIR/scripts/releases/current $TARGET_DIR/scripts/releases/previous
+cp -r $TARGET_DIR/releases/current $TARGET_DIR/releases/previous
 
 # Update current link
-ln -sfn $TARGET_DIR/scripts/releases/$TIME_TAG $TARGET_DIR/scripts/releases/current
+ln -sfn $TARGET_DIR/releases/$TIME_TAG $TARGET_DIR/releases/current
